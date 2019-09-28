2adaff324a7a56c67f279b49cbb331f50eaf11d4
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-28 23:30:10 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initialize (name, breed)
+    @name= name
+  end
+  def name= (name)
+    @name= name
+  end
+  def name
+    @name
+  end
+  def initialize (breed)
+    @breed= breed
+  end
+  def breed= (breed)
+    @breed= breed
+  end
+  def breed
+    @breed
+  end
+end
+#instance_variable_get(:@name)
+#instance_variable_get(:@breed)
+#mutt.instance_variable_set(:@breed, "Mutt")
